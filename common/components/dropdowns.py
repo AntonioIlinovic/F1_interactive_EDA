a1ee34a752d37ae8b1cf7f54d860418640f1ffa1
AntonioIlinovic <antonio.ilinovic1@gmail.com>
2024-01-19 01:17:56 +0100

    Histogram Distribution layout now uses dropdown element from common components

--- common/components/dropdowns.py
+++ common/components/dropdowns.py
@@ -1,12 +1,13 @@
 from dash import dcc
 
 
-def create_dropdown(html_id='dropdown', options=None, value=None):
+def create_dropdown(html_id='dropdown', options=None, value=None, placeholder=None):
     if options is None:
         options = []
     return dcc.Dropdown(
         id=html_id,
         className='dropdown',
         options=options,
-        value=value
+        value=value,
+        placeholder=placeholder,
     )
--- pages/distribution_histogram/layout.py
+++ pages/distribution_histogram/layout.py
@@ -1,4 +1,6 @@
 from dash import html, dcc
+
+from common.components import dropdowns
 from common.utils.utils import DatasetName
 
 
@@ -9,24 +11,20 @@ def distribution_histogram():
 
 
 def distribution_dataset_selector():
-    return html.Div([
-        dcc.Dropdown(
-            value=DatasetName.DRIVERS.value,  # default value
-            id='distribution-histogram-dataset-dropdown',
-            options=[{'label': dataset_name.value, 'value': dataset_name.value} for dataset_name in DatasetName],
-            placeholder='Select a Dataset',
-        ),
-    ])
+    return dropdowns.create_dropdown(
+        value=DatasetName.DRIVERS.value,  # default value
+        html_id='distribution-histogram-dataset-dropdown',
+        options=[{'label': dataset_name.value, 'value': dataset_name.value} for dataset_name in DatasetName],
+        placeholder='Select a Dataset'
+    )
 
 
 def distribution_column_selector():
-    return html.Div([
-        dcc.Dropdown(
-            value='dob',  # default value, date of birth
-            id='distribution-histogram-column-dropdown',
-            placeholder='Select a Column',
-        ),
-    ])
+    return dropdowns.create_dropdown(
+        value='dob',  # default value, date of birth
+        html_id='distribution-histogram-column-dropdown',
+        placeholder='Select a Column'
+    )
 
 
 def distribution_histogram_layout():
